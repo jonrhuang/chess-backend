84eb433b799632e794125333ab6555835013c138
jonrhuang <jonrhuang@gmail.com>
2026-03-28 14:15:05 -0400

    Move functionality added

--- src/main/java/com/example/chess/controller/ChessController.java
+++ src/main/java/com/example/chess/controller/ChessController.java
@@ -1,19 +1,51 @@
 package com.example.chess.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.chess.service.GameService;
+import com.example.chess.model.Board;
+import com.example.chess.model.MoveRequest;
 
 @RestController
 public class ChessController {
+
+  private final GameService gameService;
+
+  public ChessController(GameService gameService) {
+    this.gameService = gameService;
+  }
 
   @GetMapping("/")
   public String home() {
     return "Home page";
   }
 
+  @GetMapping("/board")
+  public Board getBoard() {
+    return gameService.getBoard();
+  }
+
   @GetMapping("/hello")
   public String hello() {
     return "chess server running";
   }
+
+  @PostMapping("/move")
+  public Board move(
+    @RequestBody MoveRequest move
+  ) {
+    gameService.movePiece(
+      move.getStartRow(),
+      move.getStartCol(),
+      move.getEndRow(),
+      move.getEndCol()
+    );
+
+    return gameService.getBoard();
+  }
+  
 
 }
---
+++ src/main/java/com/example/chess/model/MoveRequest.java
@@ -0,0 +1,28 @@
+package com.example.chess.model;
+
+public class MoveRequest {
+  private int startRow; 
+  private int startCol; 
+  private int endRow; 
+  private int endCol; 
+
+  public MoveRequest(int startRow, int startCol, int endRow, int endCol) {
+    this.startRow = startRow;
+    this.startCol = startCol;
+    this.endRow = endRow;
+    this.endCol = endCol;
+  }
+
+  public int getStartRow() {
+    return startRow;
+  }
+  public int getStartCol() {
+    return startCol;
+  }
+  public int getEndRow() {
+    return endRow;
+  }
+  public int getEndCol() {
+    return endCol;
+  }
+}
